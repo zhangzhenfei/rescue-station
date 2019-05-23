29fd1a4efaf0acf2885b8dbac9d877246b7c971f
fuwenlong <long@sadais.com>
2019-05-23 21:15:06 +0800

    登录

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,1 +1,7 @@
+const API_HOST = 'https://api.sadais.com'
 const FILE_HOST = 'http://m.sadais.com/rescue/img/app_h5/'
+
+export default {
+  API_HOST,
+  FILE_HOST
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,18 @@
 import { ComponentType } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Image } from '@tarojs/components'
-import { AtForm, AtInput, AtButton } from 'taro-ui'
+import { AtForm, AtInput, AtButton, AtMessage } from 'taro-ui'
 
 const styles = require('./index.module.scss')
-
-type PageStateProps = {}
-
-interface Index {
-  props: PageStateProps
-}
 
 class Index extends Component {
   config: Config = {
     navigationBarTitleText: '欢迎页'
+  }
+
+  public state = {
+    account: '',
+    password: ''
   }
 
   componentWillMount() {}
@@ -28,13 +27,45 @@ class Index extends Component {
 
   componentDidHide() {}
 
+  handleInputChange(type, val) {
+    console.log(`type:${type};val:${val}`)
+    this.setState({
+      [type]: val
+    })
+  }
+
+  validateForm() {
+    const { account, password } = this.state
+    if (!account) {
+      Taro.atMessage({
+        message: '请输入账号',
+        type: 'warning'
+      })
+      return false
+    }
+
+    if (!password) {
+      Taro.atMessage({
+        message: '请输入密码',
+        type: 'warning'
+      })
+      return false
+    }
+    return true
+  }
+
   login() {
-    Taro.switchTab({ url: '/pages/index/index' })
+    if (this.validateForm()) {
+      Taro.switchTab({ url: '/pages/index/index' })
+    }
+    // Taro.switchTab({ url: '/pages/index/index' })
   }
 
   render() {
+    const { account, password } = this.state
     return (
       <View className="container">
+        <AtMessage />
         <View className={styles.contentWrap}>
           <View className={styles.content}>
             <Image
@@ -45,16 +76,30 @@ class Index extends Component {
             />
             <div className={styles.title}>智能应急综合救援站</div>
             <AtForm className={styles.form}>
-              <AtInput name="value" type="text" placeholder="请输入账号" />
-              <AtInput name="value3" type="password" placeholder="请输入密码" />
+              <AtInput
+                className={styles.input}
+                name="account"
+                value={account}
+                onChange={this.handleInputChange.bind(this, 'account')}
+                placeholder="请输入账号"
+              />
+              <AtInput
+                className={styles.input}
+                name="password"
+                value={password}
+                type="password"
+                onChange={this.handleInputChange.bind(this, 'password')}
+                placeholder="请输入密码"
+              />
               <AtButton
                 type="primary"
                 formType="submit"
+                onClick={this.login.bind(this)}
                 className={styles.submit}
-                onClick={this.login}
               >
                 登录
               </AtButton>
+              {/* disabled={!account || !password} */}
             </AtForm>
           </View>
         </View>
