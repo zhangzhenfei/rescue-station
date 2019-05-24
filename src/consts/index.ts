3dec38f10f57be5106ca32411fbc18bc6db1a489
zhangzhenfei <fei@sadais.com>
2019-05-24 10:06:23 +0800

    bug fix

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,5 +1,5 @@
-const API_HOST = 'https://api.sadais.com'
-const FILE_HOST = 'http://m.sadais.com/rescue/img/app_h5/'
+export const API_HOST = 'https://api.sadais.com'
+export const FILE_HOST = 'http://m.sadais.com/rescue/img/app_h5/'
 
 export default {
   API_HOST,
